6263f72232caf6073eca94878516b69c1d208b9d
Clinzho1996 <confidinho@yahoo.com>
2025-03-21 07:26:09 +0100

    phone ups

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -134,7 +134,7 @@ function Home() {
 							className="w-full bg-[#9F9E9E29] rounded-lg p-2 border border-[#0000001A] focus:outline-none focus:border-primary mt-2 placeholder:text-sm placeholder:text-[#6B7280] placeholder:font-inter shadow-inner"
 						/>
 						<p className="text-xs text-[#6B7280] font-inter font-light mt-3">
-							Phone Number (optional)
+							Phone Number 
 						</p>
 						<input
 							placeholder="+84 80 0000 00000"

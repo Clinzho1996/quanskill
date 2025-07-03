80a9e8258e8cba21e926dada93ad78ae41791d79
Confidence Emonena <confidinho@yahoo.com>
2025-07-03 12:45:02 +0100

    content update

--- components/Learning.tsx
+++ components/Learning.tsx
@@ -1,6 +1,15 @@
 import LearnCard from "./LearnCard";
 
 function Learning() {
+	const handleOpenWhatsApp = () => {
+		const phoneNumber = "+84942322620"; // Replace with the actual phone number
+		const message =
+			"Hello, I would like to know more about the learning resources.";
+		const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+			message
+		)}`;
+		window.open(url, "_blank");
+	};
 	return (
 		<div className="mt-2 w-full">
 			<hr className="border-[1px] border-[#F1F1F1] mt-10 mb-10" />
@@ -25,6 +34,13 @@ function Learning() {
 					description="Resources available anytime, anywhere."
 					bg="/library.png"
 					handleClick={() => {}}
+				/>
+				<LearnCard
+					img="/library-icon.png"
+					title="One-on-One Mentorship"
+					description="Get personalized guidance from industry experts."
+					bg="/library.png"
+					handleClick={() => handleOpenWhatsApp()}
 				/>
 			</div>
 		</div>
